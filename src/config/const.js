308976e53ea1ec39fc558ce630e64e7ee21f0c65
yys1105 <1791566096@qq.com>
2017-11-16 12:00:25 +0800

    feat:项目初始化

--- src/config/const.js
+++ src/config/const.js
@@ -1,1 +1,1 @@
-export const BASE_URL = '';
+export const BASE_URL = 'http://www.easy-mock.com/mock/5a0cfe287607e8553dac7d83';
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -8,17 +8,29 @@ export default {
       }
   },
   methods: {
+    login(){
+      this.$httpPost('/api/login',{
 
+      }).then((data)=>{
+        console.log(data)
+        this.$vux.toast.text(data.message,'middle')
+      }).catch(err=>{
+        this.$vux.toast.text(err.message,'middle')
+      })
+    }
   },
   created () {
-
+    this.login()
   }
 }
 </script>
 
 <template>
-    <div>new</div>
+    <div class="link">new</div>
 </template>
 
 <style lang="stylus" scoped>
+  @import "../../assets/style/variables.styl"
+  .link
+    color $link
 </style>
