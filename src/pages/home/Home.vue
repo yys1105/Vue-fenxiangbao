ceb3fe2393d6d08f885cc7c249ff81660876f628
yys1105 <1791566096@qq.com>
2017-11-20 16:17:00 +0800

    fix:安装依赖出错

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,62 +1,161 @@
 <script>
   import SearchBar from './_SearchBar.vue'
   import Swiper from './_Swiper.vue'
-  import { Group, Flexbox, FlexboxItem } from 'vux'
+  import Goods from './_Goods.vue'
+  import {Group, Flexbox, FlexboxItem, Sticky} from 'vux'
+  import MeScroll from 'mescroll.js'
 
   export default {
     name: 'home',
     components: {
       SearchBar,
       Swiper,
+      Goods,
       Group,
       Flexbox,
-      FlexboxItem
+      FlexboxItem,
+      Sticky
     },
     data() {
-      return {}
+      return {
+        goodsBoxHeight: 0,
+        linkList: [{
+          imgSrc: '/static/images/home_fenxiangbao.png',
+          text: '分享宝',
+          linkToName: 'Home'
+        }, {
+          imgSrc: '/static/images/home_baoleba.png',
+          text: '报了吧',
+          linkToName: 'Home'
+        }, {
+          imgSrc: '/static/images/home_changlegou.png',
+          text: '厂乐购',
+          linkToName: 'Home'
+        }, {
+          imgSrc: '/static/images/home_register.png',
+          text: '注册',
+          linkToName: 'Home'
+        }],
+        goodsList: [],
+        totalPage: 0
+      }
     },
     methods: {
       search(val) {
         console.log(val)
+      },
+      getListDataFromNet(pageNum, pageSize, successCallback, errorCallback) {
+        this.$httpPost("/api/getGoodsList", {
+          pageCurrent: pageNum,
+          pageSize: pageSize
+        }).then((data) => {
+          var listData = data.data.list;
+          this.totalPage = data.data.pageCount
+          successCallback && successCallback(listData);//成功回调
+        }).catch(err => {
+          this.$vux.toast.text(err.message, 'middle')
+          errorCallback && errorCallback()
+        });
+      },
+      upCallback: function (page) {
+        var self = this;
+        self.getListDataFromNet(page.num, page.size, function (curPageData) {
+//          curPageData=[]
+          if (page.num == 1) self.dataList = [];
+          self.goodsList = self.goodsList.concat(curPageData);
+          self.mescroll.endByPage(curPageData.length, self.totalPage); //必传参数(当前页的数据个数, 总页数)
+        }, function () {
+          self.mescroll.endErr();
+        });
       }
     },
     created() {
+      this.goodsBoxHeight = document.body.clientHeight - 46 - 53 - 93
+    },
+    mounted: function () {
+      var self = this;
+      self.mescroll = new MeScroll("mescroll", {
+        down: {
+          offset: 50,
+          minAngle: 80
+        },
+        up: {
+          callback: self.upCallback,
+          toTop: {
+            src: "/static/images/mescroll-totop.png",
+          },
+          empty: {
+            warpId: "mescroll",
+            icon: "",
+            tip: "亲,暂无相关数据哦~",
+          },
+        }
+      });
     }
   }
 </script>
 
 <template>
-  <div>
-    <div>
+  <div id="vux_view_box_body">
+    <sticky scroll-box="vux_view_box_body" :offset="0" style="z-index: 999">
       <search-bar @search="search"></search-bar>
-    </div>
+    </sticky>
     <div>
       <swiper></swiper>
     </div>
-    <group :gutter="10">
-      <flexbox class="icon-link">
-        <flexbox-item>
-          <div class="icon-link-wrap">
-            <div class="icon-img">
-              <img src="/static/images/home_fenxiangbao.png">
+
+    <sticky scroll-box="vux_view_box_body" :offset="46">
+      <flexbox :gutter="0" class="icon-link">
+        <template v-for="item in linkList">
+          <flexbox-item>
+            <div class="icon-link-wrap" @click="$router.push({name:item.linkToName})">
+              <div class="icon-img">
+                <img :src="item.imgSrc">
+              </div>
+              <div class="link-text">
+                {{ item.text }}
+              </div>
             </div>
-          </div>
-        </flexbox-item>
-        <flexbox-item><div>2</div></flexbox-item>
-        <flexbox-item><div>3</div></flexbox-item>
-        <flexbox-item><div>4</div></flexbox-item>
+          </flexbox-item>
+        </template>
       </flexbox>
-    </group>
+    </sticky>
+
+
+      <div :style="{'height':goodsBoxHeight+'px'}" style="overflow: hidden">
+
+        <div id="mescroll" class="mescroll">
+          <flexbox :gutter="0" class="goods-wrap" wrap="wrap">
+            <template v-for="item in goodsList">
+              <flexbox-item :span="1/2">
+                <goods :data="item"></goods>
+              </flexbox-item>
+            </template>
+          </flexbox>
+        </div>
+
+
+      </div>
   </div>
 </template>
 
 <style lang="stylus" scoped>
   @import "../../assets/style/variables.styl"
+  @import '/static/style/mescroll.min.css'
   .icon-link
+    background-color #fff
+    margin-top 10px
     .icon-link-wrap
-      padding .13rem .2rem
+      padding .1rem .23rem
     .icon-img
       width 100%
-      &>img
+      & > img
         width 100%
+    .link-text
+      text-align center
+      font-size .1rem
+      color $lightDark
+
+  .goods-wrap
+    margin-top 3px
 </style>
--- src/pages/home/_SearchBar.vue
+++ src/pages/home/_SearchBar.vue
@@ -1,4 +1,5 @@
 <script>
+//  import {Search} from 'vux'
   export default {
     name: 'searchBar',
     data() {
@@ -14,6 +15,12 @@
       cancel(){
         this.searching = false
         this.value = ''
+      },
+      clickSearch(){
+        this.searching = true
+        this.$nextTick(() => {
+          this.$refs.input.focus()
+        })
       }
     }
   }
@@ -21,12 +28,12 @@
 
 <template>
   <div class="search-bar-wrap">
-    <div class="search-container">
-      <div class="default-text" v-if="!searching" @click="searching = true">
+    <div class="search-container" :class="{'dark-bg':searching}">
+      <div class="default-text" v-show="!searching" @click="clickSearch">
         <i class="iconfont icon-sousuo"></i>&nbsp;搜索
       </div>
-      <div class="input-wrap" v-if="searching">
-        <input type="text" class="input" ref="input" v-model="value" autofocus @keyup.13="search">
+      <div class="input-wrap" v-show="searching">
+        <input type="text" class="input" ref="input" v-model="value" @keyup.13="search">
         <span class="cancel-text" @click="cancel">取消</span>
       </div>
     </div>
@@ -49,7 +56,8 @@
         color $sky-blue
         font-size .14rem
       .input-wrap
-        padding .03rem .45rem .03rem .15rem
+        padding 0 .45rem 0 .15rem
+        height .27rem
         position relative
         .cancel-text
           position absolute
@@ -58,8 +66,15 @@
           font-size .14rem
           color $sky-blue
         input
-          border-style none
-          outline none
+          padding 4px 0
           width 100%
-          height 100%
+          height 1.42857143em
+          border 0
+          font-size 14px
+          line-height 1.42857143em
+          box-sizing content-box
+          background transparent
+          outline none
+  .dark-bg
+    background-color #f0f0f0 !important
 </style>
